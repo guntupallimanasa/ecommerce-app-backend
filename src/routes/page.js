5ef522bfcc96988f05ddf12a85f4ba71977719b6
manasa <manusankar410@gmail.com>
2022-04-15 20:40:38 +0530

    fixed all bugs

--- src/routes/page.js
+++ src/routes/page.js
@@ -26,13 +26,13 @@ router.post('/createPage', upload.fields([
 
     if (banners.length > 0) {
         req.body.banners = banners.map((banner, index) => ({
-            img: `${process.env.API}/public/${banner.filename}`,
+            img: `/public/${banner.filename}`,
             navigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`
         }))
     }
     if (products.length > 0) {
         req.body.products = products.map((product, index) => ({
-            img: `${process.env.API}/public/${product.filename}`,
+            img: `/public/${product.filename}`,
             navigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`
         }))
     }
